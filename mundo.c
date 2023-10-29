98e7b80a5f29afa5aa8224e164b92feeb4c63961
NathalyAngamarca <nathaly.angamarca@unl.edu.ec>
2023-10-28 20:29:35 -0500

    Agregue lineas para ingresar nombre

--- mundo.c
+++ mundo.c
@@ -3,5 +3,9 @@
 #include<conio.h>
 int main()
 {
+	char nombre[50];
 	printf("Hola Mundo!!!");
+	printf("Por favor, ingresa tu nombre: ");
+    scanf("%s", nombre);  // Escaneamos el nombre del usuario
+    printf("Hola, %s. ¡Bienvenido!\n", nombre);
 }
